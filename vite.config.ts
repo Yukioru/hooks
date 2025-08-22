20a956505ab2ad01b5438d62090e1856a9d8561a
Andrey Kondalov <andrey@kondalov.com>
2025-08-22 17:16:01 +0400

    fix build d.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      exclude: ['src/stories'],
+      entryRoot: 'src',
+      outDir: 'dist',
+      insertTypesEntry: true,
+      tsconfigPath: './tsconfig.lib.json',
+    }),
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
