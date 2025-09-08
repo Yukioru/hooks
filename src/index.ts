a86117278ff2b0649800db359e2dc77e5c69a795
Andrey Kondalov <andrey@kondalov.com>
2025-09-08 13:15:36 +0400

    fix build subtypes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { useSidebar } from './useSidebar';
+export type { UseSidebarOptions, UseSidebarReturn } from './useSidebar';
+
 export { useClickOutside } from './useClickOutside';
+export type { UseClickOutsideOptions } from './useClickOutside';
+
 export { useContainerWidth } from './useContainerWidth';
